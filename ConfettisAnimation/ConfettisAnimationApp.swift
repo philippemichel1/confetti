7986a7f0da62c987adac7122f7fb66cdd8609754
Philippe MICHEL <philippe.michel1@gmail.com>
2024-05-20 19:15:13 +0200

    Initial Commit

---
+++ ConfettisAnimation/ConfettisAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfettisAnimationApp.swift
+//  ConfettisAnimation
+//
+//  Created by Philippe MICHEL on 20/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ConfettisAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
