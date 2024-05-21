ed84f7a303e53c25058a96c01fa7dffad4140a91
Philippe MICHEL <philippe.michel1@gmail.com>
2024-05-21 22:05:22 +0200

    commentaire

--- ConfettisAnimation/ViewModel/Conffeti.swift
+++ ConfettisAnimation/ViewModel/Conffeti.swift
@@ -25,16 +25,27 @@ class Confetti: CAEmitterCell {
         contents = images[Int.random(in: 0..<images.count)].cgImage
         // génére une couleur RGB pour chaque formes
         color = UIColor(red: randomValueColor(), green: randomValueColor(), blue: randomValueColor(), alpha: 1).cgColor
+        // taux affichage
         birthRate = 4
+        // durée de vie
         lifetime = 10
+        // durée de vie
         lifetimeRange = 3
+        // vitesse
         velocity = 100
+        // plage du vitesse
         velocityRange = 100
+        // rotation
         spin = 4
+        // vitesse de rotation
         spinRange = 4
+        // echelle de taille
         scale = 0.25
+        // plage par rapport à la taille
         scaleRange = 0.7
+        // direction
         emissionLongitude = CGFloat(Double.pi)
+        
         emissionRange = 0.75
     }
     
